efa8e4167360a5013792935289e741503b9491cb
Sara Pages <144797101+SPRDesign@users.noreply.github.com>
2024-01-27 22:03:09 +0000

    design2

--- src/js/component/ContactCard.js
+++ src/js/component/ContactCard.js
@@ -7,29 +7,34 @@ export const ContactCard = ({ contact }) => {
 
   return (
     <div className="contact-card col-xl-4 col-sm-6 mb-3">
-      <div className="bg-white rounded shadow-sm py-5 px-4">
+      <div className="bg-white rounded py-4 px-4 contact-card-border">
         <div className="text-center">
-          <img
-            src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png"
-            alt=""
-            width="100"
-            className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
-          />
-          <h5 className="mb-0">{full_name}</h5>
-        </div>
+          
+          <div class="image-circle">
+            <div class="profile-icon-container" >
+              <img
+                src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png"
+                alt="profile-icon"
+                width="100"
+                className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+              />
+            </div>
+          </div>
+          <h5 className="mb-0 custom-text-right">{full_name}</h5>
+          </div>
 
         <div className="row mt-3  icons-margin-left ">
           <div className="col-12">
             <div className="d-flex mb-2">
-              <i className="fas fa-at mr-2 "></i>
+              <i className="fas fa-at mr-2 icons"></i>
               <p className="mb-0 text-margin-left">{email}</p>
             </div>
             <div className="d-flex mb-2">
-              <i className="fas fa-map-marker-alt mr-2"></i>
+              <i className="fas fa-map-marker-alt mr-2 icons"></i>
               <p className="mb-0 text-margin-left ">{address}</p>
             </div>
             <div className="d-flex mb-2">
-              <i className="fas fa-mobile-alt mr-2"></i>
+              <i className="fas fa-mobile-alt mr-2 icons"></i>
               <p className="mb-0 text-margin-left ">{phone}</p>
             </div>
           </div>
@@ -37,12 +42,12 @@ export const ContactCard = ({ contact }) => {
 
         <div className="row mt-3">
           <div className="col-md-6">
-            <button className="btn btn-primary" style={{ borderColor: "transparent" }}>
-              <i className="far fa-edit" /> 
+            <button className="btn btn-primary icon-button">
+              <i className="far fa-edit" />
             </button>
           </div>
           <div className="col-md-6">
-            <button className="btn btn-danger" style={{ borderColor: "transparent" }} onClick={() => actions.deleteContacts(contact.id)}>
+            <button className="btn btn-danger icon-button" onClick={() => actions.deleteContacts(contact.id)}>
               <i className="fas fa-trash-alt" />
             </button>
           </div>
